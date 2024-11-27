10923b02d487c22c9fbafb0e861141909e272a0d
jumaniyozov <jumaniyozovse@gmail.com>
2024-11-27 17:52:27 +0500

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import {Metadata} from "next";
+
+
+
+export default function Home() {
+  return (
+    <main className="p-5">
+      <h1 className="text-4xl font-bold">Welcome to GPT Chat</h1>
+    </main>
+  );
+}
