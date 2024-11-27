2cc2d1b5c2c2e44b112ec238ac27518649c7c7a1
jumaniyozov <jumaniyozovse@gmail.com>
2024-11-27 17:55:42 +0500

    fixes for unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import {Metadata} from "next";
-
-
-
 export default function Home() {
-  return (
-    <main className="p-5">
-      <h1 className="text-4xl font-bold">Welcome to GPT Chat</h1>
-    </main>
-  );
+    return (
+        <main className="p-5">
+            <h1 className="text-4xl font-bold">Welcome to GPT Chat</h1>
+        </main>
+    );
 }
